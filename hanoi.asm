8c89fc0272a623e3e8ec9096e11dc2386c13dadc
VitorBrust1 <dead@thinkpad.localdomain>
2021-08-06 14:11:02 -0300

    Update

--- hanoi.asm
+++ hanoi.asm
@@ -85,7 +85,45 @@ naoUm:
     pop r1
     pop r0
 
+    chamada_recursiva_1:
+    ;; Parâmetros de chamada de função [r0 = n, r5 = from_rod, r6 = to_rod, r7 = aux_rod]
+    ;; Trocando os parametros de ordem para nova chamada
+    mov r0, r2
+    mov r4, r6
+    mov r6, r7
+    mov r7, r4
+    ;; talvez tenha que dar um mov entre os registradores
+    ;; loadn r5, #'A'
+    ;; loadn r6, #'C'
+    ;; loadn r7, #'B'
+    ;; Chama função recursiva
+    call torreHanoi
 
+    ;; Caso n == 1 exibe string
+    loadn r1, #moveUmP
+    call printLinha
+
+    ;; Exibe from_rod no local correto
+    load r0, cursor
+    loadn r1, #16
+    sub r0, r0, r1
+    outchar r5, r0
+    inc r0
+
+    chamada_recursiva_2:
+    ;; Parâmetros de chamada de função [r0 = n, r5 = from_rod, r6 = to_rod, r7 = aux_rod]
+    mov r0, r2
+    ;; Trocando os parametros de ordem para nova chamada
+    mov r4, r5
+    mov r5, r7
+    mov r7, r4
+    ;; loadn r5, #'A'
+    ;; loadn r6, #'C'
+    ;; loadn r7, #'B'
+    ;; Chama função recursiva
+    call torreHanoi
+
+    ;; Talvez o rts tenha que ser na funcao de cima?
     rts
 
 leNum:
